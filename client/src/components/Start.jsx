396116de072b6159d0a072fcdef3bea3bfc8b19c
Brandon Wang <sourbrandon@gmail.com>
2019-05-14 00:15:51 -0700

    fixed location querying

--- client/src/components/Start.jsx
+++ client/src/components/Start.jsx
@@ -1,39 +1,39 @@
 import React from 'react'
-import {geolocated} from 'react-geolocated';
+import Geolocation from 'react-geolocation'
 
 import Waffler from './Waffler.jsx'
 
 class Start extends React.Component {
     render() {
-        var coords = this.props.coords;
         var locationQuery = this.props.location.state.query;
-        if (coords !== null) {
-            return (
-              <div>
-                <Waffler coords={ coords }/>
-              </div>
-            );
-        } else if (locationQuery !== "") {
-            return (
-              <div>
-                <Waffler city={ locationQuery }/>
-              </div>
-            );
-        } else {
-            return (
-                <div>
-                    <div className="row justify-content-center mb-4">
-                        <h4>Searching for location</h4>
-                    </div>
-                </div>
-            );
-        }
+        return (
+            <div>
+                {locationQuery === "" ? (
+                    <Geolocation
+                        render={({
+                            fetchingPosition,
+                            position: { coords: { latitude, longitude } = {} } = {},
+                            error,
+                            getCurrentPosition
+                          }) =>
+                            <div onLoad={getCurrentPosition}>
+                                {error && <div>{error.message}</div>}
+                                {latitude ? (
+                                    <Waffler coords={ {latitude: latitude, longitude: longitude} }/>
+                                    ) : (
+                                    <div className="row justify-content-center mb-4">
+                                        <h4>Searching for location...</h4>
+                                    </div>
+                                )}
+                            </div>
+                        }
+                    />
+                ) : (
+                    <Waffler city={ locationQuery }/>
+                )}
+            </div>
+        );
     }
 }
 
-export default geolocated({
-    positionOptions: {
-        enableHighAccuracy: false,
-    },
-        userDecisionTimeout: 5000,
-    })(Start);
+export default Start;
